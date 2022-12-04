90f164fa70f6349dfff4a4a00f55c591dc87816a
Kyle Harris <kaharrs2@gmail.com>
2022-12-04 16:11:48 -0700

    Cleaned up a bit

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,4 @@
 use scan_fmt::scan_fmt;
-use std::collections::HashSet;
 
 #[aoc_generator(day4)]
 pub fn load_input(input: &str) -> Vec<(i32, i32, i32, i32)> {
@@ -11,52 +10,27 @@ pub fn load_input(input: &str) -> Vec<(i32, i32, i32, i32)> {
 }
 
 fn contain_other(e1min: i32, e1max: i32, e2min: i32, e2max: i32) -> bool {
-    if e1min <= e2min && e1max >= e2max {
-        true
-    } else if e2min <= e1min && e2max >= e1max {
-        true
-    } else {
-        false
-    }
+    (e1min <= e2min && e1max >= e2max) || (e2min <= e1min && e2max >= e1max)
 }
 
 fn overlap_at_all(e1min: i32, e1max: i32, e2min: i32, e2max: i32) -> bool {
-    let mut range1 = HashSet::new();
-    for i in e1min..e1max + 1 {
-        range1.insert(i);
-    }
-    let mut range2 = HashSet::new();
-    for i in e2min..e2max + 1 {
-        range2.insert(i);
-    }
-    let thing: Vec<_> = range1.intersection(&range2).collect();
-    if thing.len() >= 1 {
-        true
-    } else {
-        false
-    }
+    !(e1max < e2min || e2max < e1min)
 }
 
 #[aoc(day4, part1)]
-pub fn part1(input: &[(i32, i32, i32, i32)]) -> u32 {
-    let mut cntr = 0;
-    for line in input {
-        if contain_other(line.0, line.1, line.2, line.3) {
-            cntr += 1;
-        }
-    }
-    cntr
+pub fn part1(input: &[(i32, i32, i32, i32)]) -> usize {
+    input
+        .iter()
+        .filter(|line| contain_other(line.0, line.1, line.2, line.3))
+        .count()
 }
 
 #[aoc(day4, part2)]
-pub fn part2(input: &[(i32, i32, i32, i32)]) -> u32 {
-    let mut cntr = 0;
-    for line in input {
-        if overlap_at_all(line.0, line.1, line.2, line.3) {
-            cntr += 1;
-        }
-    }
-    cntr
+pub fn part2(input: &[(i32, i32, i32, i32)]) -> usize {
+    input
+        .iter()
+        .filter(|line| overlap_at_all(line.0, line.1, line.2, line.3))
+        .count()
 }
 
 #[cfg(test)]
