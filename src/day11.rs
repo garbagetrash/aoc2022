12a6244dd32758fb161ef0f5834d0854acc6f13a
Kyle Harris <kaharrs2@gmail.com>
2022-12-11 01:48:44 -0700

    finished day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,12 +1,6 @@
 use scan_fmt::scan_fmt;
 
-#[derive(Debug, Copy, Clone)]
-enum Command {
-    Addx(i64),
-    Noop,
-}
-
-type Input = Poop;
+type Input = Monkey;
 
 #[derive(Copy, Clone, Debug)]
 enum OpArg {
@@ -15,130 +9,121 @@ enum OpArg {
 }
 
 #[derive(Debug)]
-struct Poop {
-    mnum: usize,
-    sitems: Vec<usize>,
+struct Monkey {
+    items: Vec<usize>,
     op: char,
     oparg: OpArg,
-    divby: usize,
-    mtruth: usize,
-    mfalse: usize,
+    divisor: usize,
+    truth_id: usize,
+    false_id: usize,
 }
 
 #[aoc_generator(day11)]
 fn load_input(input: &str) -> Vec<Input> {
     let mut output = vec![];
-    let mut i = 0;
-    let mut mnum = 0;
-    let mut sitems: Vec<usize> = vec![];
-    let mut n_op = '+';
-    let mut oparg = OpArg::Old;
-    let mut divby = 0;
-    let mut truth_monkey = 0;
-    let mut false_monkey = 0;
-    for line in input.lines() {
-        match i % 7 {
-            0 => {
-                mnum = scan_fmt!(line, "Monkey {}:", usize).unwrap();
+    let mut lines = input.lines();
+    loop {
+        lines.next(); // Monkey #:
+        let temp: Vec<_> = lines.next().unwrap().split(": ").collect();
+        let items: Vec<_> = temp[1]
+            .split(", ")
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+        let (op, num) = scan_fmt!(
+            lines.next().unwrap(),
+            "  Operation: new = old {} {}",
+            char,
+            String
+        )
+        .unwrap();
+        let oparg = if num != "old" {
+            OpArg::Num(num.parse::<usize>().unwrap())
+        } else {
+            OpArg::Old
+        };
+        let divisor = scan_fmt!(lines.next().unwrap(), "  Test: divisible by {}", usize).unwrap();
+        let truth_id = scan_fmt!(
+            lines.next().unwrap(),
+            "    If true: throw to monkey {}",
+            usize
+        )
+        .unwrap();
+        let false_id = scan_fmt!(
+            lines.next().unwrap(),
+            "    If false: throw to monkey {}",
+            usize
+        )
+        .unwrap();
+        output.push(Monkey {
+            items: items.clone(),
+            op,
+            oparg,
+            divisor,
+            truth_id,
+            false_id,
+        });
+        if lines.next().is_none() {
+            break;
+        }
+    }
+    output
+}
+
+fn monkey_inspection(
+    monkey: &Monkey,
+    items: &[usize],
+    modulo: Option<usize>,
+) -> Vec<(usize, usize)> {
+    let mut output = vec![];
+    for item in items {
+        let op_arg: usize = match monkey.oparg {
+            OpArg::Old => *item,
+            OpArg::Num(nn) => nn,
+        };
+        let mut new = *item;
+        match monkey.op {
+            '+' => {
+                new += op_arg;
             }
-            1 => {
-                let asdf: Vec<_> = line.split(": ").collect();
-                sitems = asdf[1]
-                    .split(", ")
-                    .map(|s| s.parse::<usize>().unwrap())
-                    .collect();
-            }
-            2 => {
-                let (op, num) =
-                    scan_fmt!(line, "  Operation: new = old {} {}", char, String).unwrap();
-                n_op = op;
-                println!("derp: {}", num);
-                if num != "old" {
-                    oparg = OpArg::Num(num.parse::<usize>().unwrap());
-                } else {
-                    oparg = OpArg::Old;
-                }
-            }
-            3 => {
-                divby = scan_fmt!(line, "  Test: divisible by {}", usize).unwrap();
-            }
-            4 => {
-                truth_monkey = scan_fmt!(line, "    If true: throw to monkey {}", usize).unwrap();
-            }
-            5 => {
-                false_monkey = scan_fmt!(line, "    If false: throw to monkey {}", usize).unwrap();
-                output.push(Poop {
-                    mnum: mnum,
-                    sitems: sitems.clone(),
-                    op: n_op,
-                    oparg: oparg,
-                    divby: divby,
-                    mtruth: truth_monkey,
-                    mfalse: false_monkey,
-                });
-            }
-            6 => {
-            },
+            '*' => new *= op_arg,
             _ => (),
         }
-        i += 1;
+
+        if let Some(m) = modulo {
+            // part 2
+            new %= m;
+        } else {
+            // part 1
+            new /= 3;
+        }
+        let next_monkey = if new % monkey.divisor == 0 {
+            monkey.truth_id
+        } else {
+            monkey.false_id
+        };
+
+        output.push((next_monkey, new));
     }
     output
 }
 
 #[aoc(day11, part1)]
-fn part1(input: &[Input]) -> i64 {
-    let mut arr: Vec<Vec<usize>> = vec![vec![]; input.len()];
-    for (i, line) in input.iter().enumerate() {
-        println!("line: {:?}", line);
-        arr[i] = line.sitems.clone();
+fn part1(input: &[Input]) -> usize {
+    let mut monkey_arr: Vec<Vec<usize>> = vec![vec![]; input.len()];
+    for (i, monkey) in input.iter().enumerate() {
+        monkey_arr[i] = monkey.items.clone();
     }
 
-    println!("lenght: {}", input.len());
     let mut monkeys = vec![0; input.len()];
     for _ in 0..20 {
-        let mut next_arr: Vec<Vec<usize>> = vec![vec![]; monkeys.len()];
-        for (n, line) in input.iter().enumerate() {
-            println!("{:?}", line);
-            println!("items: {:?}", arr[n]);
-
-            for item in &arr[n] {
-                let op_arg: usize = match line.oparg {
-                    OpArg::Old => *item,
-                    OpArg::Num(nn) => nn,
-                };
-                let mut new = *item;
-                println!("new: {}, op_arg: {}", new, op_arg);
-                match line.op {
-                    '+' => {
-                        new += op_arg;
-                    },
-                    '*' => new *= op_arg,
-                    _ => (),
-                }
-
-                let mut next_monkey = 0;
-                new /= 3;
-                if new % line.divby == 0 {
-                    next_monkey = line.mtruth;
-                } else {
-                    next_monkey = line.mfalse;
-                }
-
-                println!("  item value {} is thrown to monkey {}", new, next_monkey);
-                next_arr[next_monkey].push(new);
-                monkeys[n] += 1;
+        for (n, monkey) in input.iter().enumerate() {
+            let output = monkey_inspection(monkey, &monkey_arr[n], None);
+            monkeys[n] += monkey_arr[n].len();
+            monkey_arr[n] = vec![];
+            for item in output {
+                monkey_arr[item.0].push(item.1);
             }
         }
-        arr = next_arr;
-
-        for m in &monkeys {
-            println!("  monkey: {:?}", m);
-        }
-        for items in &arr {
-            println!("  items: {:?}", items);
-        }
-        println!();
     }
     monkeys.sort();
     monkeys = monkeys.iter().copied().rev().collect();
@@ -146,8 +131,27 @@ fn part1(input: &[Input]) -> i64 {
 }
 
 #[aoc(day11, part2)]
-fn part2(input: &[Input]) -> i64 {
-    0
+fn part2(input: &[Input]) -> usize {
+    let mut monkey_arr: Vec<Vec<usize>> = vec![vec![]; input.len()];
+    let mut modulo = 1;
+    for (i, monkey) in input.iter().enumerate() {
+        monkey_arr[i] = monkey.items.clone();
+        modulo *= monkey.divisor;
+    }
+    let mut monkeys = vec![0; input.len()];
+    for _ in 0..10000 {
+        for (n, monkey) in input.iter().enumerate() {
+            let output = monkey_inspection(monkey, &monkey_arr[n], Some(modulo));
+            monkeys[n] += monkey_arr[n].len();
+            monkey_arr[n] = vec![];
+            for item in output {
+                monkey_arr[item.0].push(item.1);
+            }
+        }
+    }
+    monkeys.sort();
+    monkeys = monkeys.iter().copied().rev().collect();
+    monkeys[0] * monkeys[1]
 }
 
 #[cfg(test)]
@@ -166,7 +170,6 @@ mod test {
     fn test_part2() {
         let input = read_to_string("input/2022/11.txt").unwrap();
         let input = load_input(&input);
-        part2(&input);
-        assert_eq!(0, 0);
+        assert_eq!(part2(&input), 2713310158);
     }
 }
